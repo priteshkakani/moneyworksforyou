bb97fe73a32fb2d4650a2f73f4d2169da7a07dce
Pritesh Kakani <priteshgkakani@gmail.com>
2023-10-16 17:21:35 +0530

    second commit

--- stocks/admin.py
+++ stocks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Stock
+
 
 # Register your models here.
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("firstname", "lastname", "price",)
+
+
+admin.site.register(Stock, StockAdmin)
--- stocks/models.py
+++ stocks/models.py
@@ -6,3 +6,7 @@ class Stock(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     price = models.IntegerField(null=True)
+
+
+def __str__(self):
+    return f"{self.firstname}{self.lastname}"
